f5c27f058be706a027ac31c5479561abf5a20888
vivo8934 <mfundo8934@gmail.com>
2017-03-15 12:52:34 +0200

    yashelela

---
+++ registration_number.js
@@ -0,0 +1,19 @@
+var textArea = document.querySelector('.myText');
+var button = document.querySelector('.myButton')
+var Cbtn = document.querySelector('.Button')
+
+
+
+button.addEventListener('click', () => {
+  if(textArea.value > 0){
+  var node = document.createElement('li');
+  var t = document.createTextNode(textArea.value.substr(0, 2).toUpperCase() + ' ' + textArea.value.substr(2).toLowerCase());
+  node.appendChild(t);
+  document.getElementById("myList").appendChild(node);
+}
+else{
+  alert('Please enter registration_number or KINDLY CLOSE THE PRGRAM😂😂😂😂😂😂');
+}
+});
+Cbtn.addEventListener('click', () =>{
+  textArea.value = '';})
