3d5d73e180911ec973d6f3229c82f6d635897560
vivo8934 <mfundo8934@gmail.com>
2017-03-22 10:44:01 +0200

    vincent ngqanekana

--- registration_number.js
+++ registration_number.js
@@ -1,19 +1,36 @@
 var textArea = document.querySelector('.myText');
 var button = document.querySelector('.myButton')
-var Cbtn = document.querySelector('.Button')
+var doit = document.querySelector('#MyChoice')
 
 
 
 button.addEventListener('click', () => {
-  if(textArea.value.length > 0){
-  var node = document.createElement('li');
-  var t = document.createTextNode(textArea.value.substr(0, 2).toUpperCase() + ' ' + textArea.value.substr(2).toLowerCase());
-  node.appendChild(t);
-  document.getElementById("myList").appendChild(node);
+  if (textArea.value.length > 0) {
+    var node = document.createElement('li');
+    var t = document.createTextNode(textArea.value.substr(0, 2).toUpperCase() + ' ' + textArea.value.substr(2).toLowerCase());
+    node.appendChild(t);
+    document.getElementById("myList").appendChild(node);
+    textArea.value = '';
+  } else {
+    alert('Please enter registration_number or KINDLY CLOSE THE PRGRAM😂😂😂😂😂😂');
+  }
+});
+
+doit.addEventListener('change', (e) => {
+
+var Plist = document.querySelectorAll('li');
+    //  document.getElementById("myDropdown").classList.toggle("show");
+    //  var v = document.getElementsBy("#MyChoice").value;
+    //  document.getElementById('ul');
+    for (var i = 0; i < Plist.length; i++) {
+if(e.target.value === "default"){
+  Plist[i].style.display = "block";
 }
-else{
-  alert('Please enter registration_number or KINDLY CLOSE THE PRGRAM😂😂😂😂😂😂');
+else if (Plist[i].innerHTML.startsWith(e.target.value)) {
+  Plist[i].style.display = 'block';
+}
+else {
+  Plist[i].style.display = 'none';
+}
 }
 });
-Cbtn.addEventListener('click', () =>{
-  textArea.value = '';})
